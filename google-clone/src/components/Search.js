784b7827a7bba6db35db4188c799060a28cc54c6
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-08-18 10:08:58 +0300

    I'm working on the search function. I'll continue later.

--- google-clone/src/components/Search.js
+++ google-clone/src/components/Search.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
 import { Button } from '@mui/material';
 
 const Search = () => {
+    const [input, setInput] = useState('');
+    const search = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className='search'>
             <div className='search-input'>
                 <SearchIcon className='search-inputIcon' />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
             <div className='search-buttons'>
-                <Button variant='outlined'>Google Search Button</Button>
+                <Button onClick={search} variant='outlined'>Google Search Button</Button>
                 <Button variant='outlined'>I'm feeling lucky</Button>
             </div>
         </div>
