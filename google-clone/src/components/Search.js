2e8190e8d13570090be9a67725296bc4f87a7b98
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-08-19 21:33:05 +0300

    When the search button is clicked, the search page is redirected using useNavigate

--- google-clone/src/components/Search.js
+++ google-clone/src/components/Search.js
@@ -3,24 +3,29 @@ import './Search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
     const [input, setInput] = useState('');
+    const navigate = useNavigate();
     const search = (e) => {
         e.preventDefault();
+        console.log("I hit the search button");
+        navigate(`/search`);
+
     }
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='search-input'>
                 <SearchIcon className='search-inputIcon' />
                 <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
             <div className='search-buttons'>
-                <Button onClick={search} variant='outlined'>Google Search Button</Button>
+                <Button type='submit' onClick={search} variant='outlined'>Google Search Button</Button>
                 <Button variant='outlined'>I'm feeling lucky</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
